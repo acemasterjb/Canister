1b07dca3a8ee1b15abe168dc576179fe257714c3
Jamil <jamil.bousquet@gmail.com>
2019-07-30 09:42:16 -0400

    Added pages function to allow the Admin user to create pages and elements/sections on those new pages.

---
+++ blog/pages/model.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+from blog.blog.model import User
+from blog import db
+
+
+class Page(db.Model):
+    """"""
+
+    path_name = db.Column(db.String(30), primary_key=True)
+    page_name = db.Column(db.TEXT, nullable=False)
+    author_id = db.Column(db.ForeignKey(User.uid), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False,
+                           default=datetime.utcnow)
+    author = db.relationship(User, lazy="joined", backref="page")
+
+class PageElement(db.Model):
+    """"""
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.ForeignKey(User.uid), nullable=False)
+    parent = db.Column(db.ForeignKey(Page.path_name), nullable=False)
+    title = db.Column(db.String(80), nullable=False)
+    body = db.Column(db.TEXT(16777216), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False,
+                           default=datetime.utcnow)
+    parent_page = db.relationship(Page, lazy="joined", backref="page")
+    author = db.relationship(User, lazy="joined", backref="pageElement")
+    
---
+++ blog/pages/page_view.py
@@ -0,0 +1,119 @@
+from flask import abort, Blueprint, g, flash
+from flask import redirect, render_template, request
+from flask import url_for
+
+from blog.auth.views import login_required
+from blog.pages.model import Page
+from blog.pages.model import PageElement
+
+bp = Blueprint('pages', __name__)
+
+def get_page(path):
+    page = Page.query.filter_by(path_name=path).first()
+
+    if page is None:
+        abort(404, "Page id {} not found".format(id))
+
+    return page
+
+@bp.route('/<string:path>', methods=('GET',))
+def page_view(path):
+
+    from blog.pages.model import PageElement
+    page = get_page(path)
+    pages = Page.query.all()
+    # name = page.path_name
+
+    page_sections = PageElement.query.filter_by(parent=page.path_name).order_by(PageElement.created_at.asc()).all()
+
+    # Placement for POST directive to create page elements
+
+    return render_template('blog/page.html',
+                            page=page, pages=pages, page_sections=page_sections)
+
+@bp.route('/create_elem', methods=('GET','POST'))
+@login_required
+def elem_new():
+
+    if request.method == 'POST':
+        title = request.form['header']
+        body = request.form['body']
+        parent = request.args.get('parent')
+        error = None
+
+        if not title:
+            error = "You need to give this page element a header."
+        
+        if error is not None:
+            flash(error)
+        else:
+            from blog import db
+            elem = PageElement(title = title, body = body, author_id = g.user.uid, parent = parent)
+            db.session.add(elem)
+            db.session.commit()
+
+            return page_view(elem.parent)
+
+    return render_template('/page/new_elem.html')
+
+@bp.route('/section/update', methods=('GET', 'POST'))
+@login_required
+def elem_update():
+    section_id = request.args.get('section')
+    page_section = PageElement.query.get(section_id)
+    
+    if request.method == 'POST':
+        title = request.form['header']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            from blog import db
+            page_section.title = title
+            page_section.body = body
+            db.session.commit()
+            return page_view(page_section.parent)
+
+    return render_template('page/elem_update.html', elem=page_section)
+    
+@bp.route('/section/<int:id>/delete', methods=('POST',))
+@login_required
+def elem_delete(id):
+    from blog import db
+    page_section = PageElement.query.get(id)
+    db.session.delete(page_section)
+    db.session.commit()
+    return page_view(page_section.parent)
+
+
+
+
+@bp.route('/new_page', methods=('GET', 'POST'))
+@login_required
+def page_new():
+
+    if request.method == 'POST':
+        path_name = request.form['path_name']
+        page_name = request.form['page_name']
+        error = None
+
+        if not path_name:
+            error = "You need to give this page name, no spaces or special characters allowed.\n"
+            error += "Underscores, numbers and '%' are permitted."
+        if error is not None:
+            flash(error)
+        else:
+            from blog import db
+            page = Page(path_name = path_name, page_name = page_name, author_id = g.user.uid)
+            db.session.add(page)
+            db.session.commit()
+
+            return page_view(path_name)
+
+    return render_template('/page/new_page.html')
+    
